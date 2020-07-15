279d84f88e0bf03821e7f9f60989dca9c3d24910
caio-gferreira <caio_gferreira@hotmail.com>
2020-07-15 13:55:59 -0300

    componenteButton

--- calculadora/src/index.js
+++ calculadora/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1>Calculadora</h1>
+    <React.StrictMode>
+      <Calculator />
+  </React.StrictMode>
+  </div>
+  ,
   document.getElementById('root')
 );
 
