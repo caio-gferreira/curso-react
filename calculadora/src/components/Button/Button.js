2bd6d34cbeb7c8d894f0b90c0c29fb86d972dce6
caio-gferreira <caio_gferreira@hotmail.com>
2020-07-15 15:17:38 -0300

    calculator stylization

--- calculadora/src/components/Button/Button.js
+++ calculadora/src/components/Button/Button.js
@@ -2,8 +2,19 @@ import React from 'react';
 import './Button.css';
 
 const calculatorButton = (props) => {
+
+    let classes = 'button ';
+
+    classes += props.operation ? 'operation' : '';
+    classes += props.double ? 'double' : '';
+    classes += props.triple ? 'triple' : '';
+
     return (
-        <button className="button"> {props.label} </button>
+        <button 
+        onClick={e => props.click && props.click(props.label)}
+        className={classes}> 
+            {props.label} 
+        </button>
     );
 };
 
