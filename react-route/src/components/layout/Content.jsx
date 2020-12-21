d389662f146d3993c6c9f49e4ef209d442b3a284
caio-gferreira <caiogfferreira@gmail.com>
2020-12-21 16:51:07 -0300

    preparing the project structure

---
+++ react-route/src/components/layout/Content.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Content.css';
+
+const Content = props => (
+    <main className="Content">
+        <h2>Component Content</h2>
+    </main>
+)
+
+export default Content;
